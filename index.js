85aa73fb2c169dfb47b90574e12eb06f67dafdaf
Amrish Kumar <ak3754732@gmail.com>
2023-01-26 22:35:15 +0530

    update

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.port ||5000;
+const PORT = 5000;
 
 //router
 app.get('/', (req, res) => {
-    console.log(res.body);
     res.send('hello world');
 })
 app.get('/user', (req, res) => {
-    console.log(res.body);
     res.send('hello user');
 })
 app.get('/ak', (req, res) => {
